021760defb9e089f34ff66d10b1296c77f790a5a
Jacky Lee <jackywhlee@gmail.com>
2015-11-01 19:44:31 +0800

    fixed landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
 
+  get 'users/new'
+
+  get 'users/create'
+
+  root to: "users#index"
+  resources :users, only: [:index, :show, :new, :create]
 end
