9d6c16a009cc5cbe6100c4dc69fc23d04b265c63
Toni Anne Romero <tonianne.romero@gmail.com>
2015-11-02 21:55:35 +0800

    basic info to populate on restaurant profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   resources :sessions, only: [:index, :new, :create, :destroy]
 
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :show]
 end
