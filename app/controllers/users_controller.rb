7575082ecc2f3ce3d51a3e62e1daf2a070c3bd9c
Jacky Lee <jackywhlee@gmail.com>
2015-11-02 16:50:09 +0800

    partial update on users create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
- def new
-  @user = User.new
- end
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
  def create
   @user = User.new user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root :to => 'pages#index'
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create]
 
   get 'login', to: 'sessions#new'
   resources :sessions, only: [:index, :new, :create, :destroy]
