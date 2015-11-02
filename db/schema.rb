04cfdfc8d8e28f21614bc711ddeb614c54315833
Jacky Lee <jackywhlee@gmail.com>
2015-11-03 00:47:13 +0800

    added seeds to restaurants, added opening and closing hour column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102101421) do
+ActiveRecord::Schema.define(version: 20151102154614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20151102101421) do
     t.string   "price_range"
     t.string   "image_url"
     t.string   "telephone"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "opening_weekday"
+    t.string   "opening_weekend"
   end
 
   create_table "users", force: :cascade do |t|
