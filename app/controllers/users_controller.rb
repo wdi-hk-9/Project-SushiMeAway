7552522c112170d4ab0fe662a8f4029bfb2b84a3
Jacky Lee <jackywhlee@gmail.com>
2015-11-02 16:10:18 +0800

    partial update on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
- def index
-  @users = User.all
- end
-
  def new
   @user = User.new
  end
@@ -10,7 +6,7 @@ class UsersController < ApplicationController
  def create
   @user = User.new user_params
   if @user.save
-   redirect_to users_path
+   redirect_to root_path
   else
    render 'new'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/index'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   root :to => 'pages#index'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:show, :new, :create]
 
   get 'login', to: 'sessions#new'
   resources :sessions, only: [:index, :new, :create, :destroy]
