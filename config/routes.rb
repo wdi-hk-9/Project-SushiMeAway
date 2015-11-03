7007cf145589e18eded79deb5d6366db55131c69
Jacky Lee <jackywhlee@gmail.com>
2015-11-03 23:36:40 +0800

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root :to => 'pages#index'
 
   get 'login', to: 'sessions#new'
-  resources :sessions,    only: [:index, :new, :create, :destroy]
+  resources :sessions,    only: [:new, :create, :destroy]
   resources :users,       only: [:index, :new, :create, :show]
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments,  only: [:create]
+  end
+
   post 'restaurants/search', to: 'restaurants#search'
 end
