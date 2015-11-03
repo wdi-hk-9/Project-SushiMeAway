20c3569f2ebc148de162d7696c96a9b386d6d91a
Jacky Lee <jackywhlee@gmail.com>
2015-11-03 20:29:32 +0800

    amended changes from pull request#17

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :district
+
+  def website_url
+    if website.include? "http://"
+      website
+    else
+      "http://#{website}"
+    end
+  end
 end
