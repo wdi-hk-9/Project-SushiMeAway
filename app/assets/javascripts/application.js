c605ab1d7db8cc9d2db90e254397a70ec49bf972
Toni Anne Romero <tonianne.romero@gmail.com>
2015-11-04 20:51:59 +0800

    changed to reverse.each for comment results. moved toggle to restaurant.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,15 +16,3 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap
-
-$(document).ready(function(){
-  // show description on restaurant porfile
-  $('.open').click(function(){
-    $('.showDescription').slideToggle('slow');
-    if($(this).text() == '(close)'){
-      $(this).text('(show more)');
-    }else{
-      $(this).text('(close)');
-    }
-  });
-});
