e19cc3e09d8258ad76f2faf310aabbbb1c53d1b7
Jacky Lee <jackywhlee@gmail.com>
2015-11-04 22:37:14 +0800

    username minimum max length, email must include @ and other rules added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, length: { minimum: 3, maximum: 30 }
+validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   has_many :comments
 end
